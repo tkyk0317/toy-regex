98808fc63c00ee78ec43f3300869687d1a5bd64a
takayuki tanaka <takayuki.1981.0317@gmail.com>
2021-11-22 09:48:40 +0900

    NFAエンジンのベンチマーク計測処理を追加

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,9 +4,11 @@
 extern crate test;
 use toy_regex::regex;
 
+static LEN: usize = 100000;
+
 #[bench]
 fn regex_vm_bench(b: &mut test::Bencher) {
-    let s = String::from_utf8(vec![b'a'; 500000000]).unwrap();
+    let s = String::from_utf8(vec![b'a'; LEN]).unwrap();
     b.iter(|| regex::Regex::new(".*a").exec(&s, true));
 }
 
@@ -22,3 +24,9 @@ fn regex_vm_bench(b: &mut test::Bencher) {
 // b.iter(|| regex::Regex::new(".*a").exec(&s, true));
 // [loop version]    test regex_vm_bench ... bench: 477,497,662 ns/iter (+/- 23,017,031)
 //--------------------------------------------------------------------------------------------
+
+#[bench]
+fn regex_nfa_bench(b: &mut test::Bencher) {
+    let s = String::from_utf8(vec![b'a'; LEN]).unwrap();
+    b.iter(|| regex::Regex::new(".*a").exec(&s, false));
+}
